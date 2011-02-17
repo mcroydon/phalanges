23d81b288509aded396275a19538e633cf2c4e21
Matt Croydon <matt@lawrence.com>
2011-02-17 16:24:46 -0600

    Added specs and mockito to build config.

--- project/build/Phalanges.scala
+++ project/build/Phalanges.scala
@@ -7,5 +7,8 @@ class Phalanges(info: ProjectInfo) extends DefaultProject(info)
     val twitterRepo = "maven.twttr.com" at "http://maven.twttr.com/"
     val configgy = "net.lag" % "configgy" % "2.0.2"
     val ostrich = "com.twitter" % "ostrich" % "3.0.4"
+    val specs = "org.specs" % "specs" % "1.6.7" from "http://specs.googlecode.com/files/specs_2.8.1-1.6.7-tests.jar"
+    val mockitoRepo = "repo2.maven.org" at "http://repo2.maven.org/maven2/org/mockito/mockito-core/"
+    val mockito = "org.mockito" % "mockito-core" % "1.8.5"
     override def mainClass = Some("com.postneo.phalanges.FingerServer")
 }
